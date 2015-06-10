218fbc05070e09cea0b91e159b6426c82e612336
Tyler Vanderhoef <tyler.j.vanderhoef@gmail.com>
2015-06-09 20:30:55 -0700

    Improved error reporting.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/// <reference path="typings/node/node.d.ts"/>
 var hapi = require('hapi'),
 	controller = ('./alexa/controller'),
 	Boom = require('boom'),
@@ -14,34 +15,45 @@ var alexaResponses = {
 };
 
 //create the server connection on the configuration port.
-server.connection({ port: 8080 });
+server.connection({ port: process.env.port || 8080 });
 
 //register all routes
 server.route([
-{
-	method: 'POST',
-	path: '/alexa',
-	config: {
-		handler: function(request, result){
-			var alexa = request.body;
-			if(alexa.application.applicationId != alexaConfig.applicationId){
-				return Boom("ApplicationId is incorrect.");
+	{
+		method: 'POST',
+		path: '/alexa',
+		config: {
+			handler: function (request, reply) {
+				var result = null;
+				try {
+					var alexa = request.body;
+					
+					if (alexa.application.applicationId != alexaConfig.applicationId) {
+						return Boom("The application id specified is incorrect for this application.", alexa);
+					}
+
+					var data = alexa.request;
+
+					if (alexa.request.type == alexaResponses.end)
+						result = controller.end(data);
+					else if (alexa.request.type == alexaResponses.intent)
+						result = controller.intent(data);
+					else if (alexa.request.type == alexaResponses.launch)
+						result = controller.launch(data);
+					else
+						result = Boom("Unknown request type recieved.");
+
+				}
+				catch (e) {
+					result = Boom.badRequest(e, {
+						route: "/alexa",
+						body: request.body
+					});
+				}
+				reply(result);
 			}
-			
-			var data = alexa.request;
-			
-			if(alexa.request.type == alexaResponses.end)
-				return controller.end(data);
-			else if(alexa.request.type == alexaResponses.intent)
-				return controller.intent(data);
-			else if(alexa.request.type == alexaResponses.launch)
-				return controller.launch(data);
-			else
-				return Boom("Unknown request type recieved.");
-			
 		}
 	}
-}
 ]);
 
 server.start(function () {
