9610348a2b95819c677c83244383a7cdae6a289c
Tyler Vanderhoef <tyler.j.vanderhoef@gmail.com>
2015-06-11 19:22:30 -0700

    Included alexa-app npm module that I forked.

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var hapi = require('hapi'),
 	helper = require('./alexa/helper'),
 	Boom = require('boom'),
 	util = require('util'),
-	server = new hapi.Server();
+	server = new hapi.Server(),
+	alexaApp = require('./alexa/alexa'),
+	alexa = new alexaApp.App('CookBook');
 
 var alexaConfig = {
 	applicationId: 'amzn1.echo-sdk-ams.app.ffa2f852-7629-49a6-8c89-c7295a1f4e7e'
@@ -19,6 +21,21 @@ var alexaResponses = {
 //create the server connection on the configuration port.
 server.connection({ port: process.env.port || 8080 });
 
+alexa.launch(function(request,response) {
+  response.say("Hello World");
+  response.card("Hello World","This is an example card");
+});
+
+alexa.intent('number',function(request,response) {
+  var number = request.slot('number');
+  response.say("You asked for the number "+number);
+});
+
+alexa.sessionEnded(function(request,response) {
+    //logout( request.userId );
+    // No response necessary
+});
+
 //register all routes
 server.route([
 	{
@@ -26,6 +43,7 @@ server.route([
 		path: '/alexa',
 		config: {
 			handler: function (request, reply) {
+				
 				var result = null;
 				try {
 					var alexa = request.payload;
@@ -33,24 +51,24 @@ server.route([
 					if (alexa.session.application.applicationId != alexaConfig.applicationId) {
 						return Boom("The application id specified is incorrect for this application.", alexa);
 					}
-
-					var data = alexa.request;
-
-					if (alexa.request.type == alexaResponses.end){
-						console.log("END");
-						result = controller.end(data);
-					}
-					else if (alexa.request.type == alexaResponses.intent){
-						console.log("INTENT");
-						result = controller.intent(data);
-					}
 					
-					else if (alexa.request.type == alexaResponses.launch){
-						console.log("LAUNCH");
-						result = controller.launch(data);
-					}
-					else
-						result = Boom("Unknown request type recieved.");
+					alexa.request(request, reply);
+
+//					if (alexa.request.type == alexaResponses.end){
+//						console.log("END");
+//						result = controller.end(data);
+//					}
+//					else if (alexa.request.type == alexaResponses.intent){
+//						console.log("INTENT");
+//						result = controller.intent(data);
+//					}
+//					
+//					else if (alexa.request.type == alexaResponses.launch){
+//						console.log("LAUNCH");
+//						result = controller.launch(data);
+//					}
+//					else
+//						result = Boom("Unknown request type recieved.");
 				}
 				catch (e) {
 					result = Boom.badRequest(e, {
