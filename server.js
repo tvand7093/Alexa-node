0b8b1936fa3bb0ed03390ed03d79250a80af4af9
Tyler Vanderhoef <tyler.j.vanderhoef@gmail.com>
2015-10-01 21:58:08 -0700

    Working website.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 var alexa = require('alexa-app');
 var app = new alexa.app('italian-parser');
 var express = require('express')();
+var bodyParser = require('body-parser');
+
+express.use(bodyParser.json());
 
 app.pre = function(request,response,type) {
     if (request.sessionDetails.application.applicationId!="amzn1.echo-sdk-ams.app.b52af693-bd89-4263-9f29-29f019d7d9a4") {
@@ -10,15 +13,15 @@ app.pre = function(request,response,type) {
     }
 };
 
-app.post = function(request,response,type,exception) {
-    // Always turn an exception into a successful response
-    response.clear().say("An error occured: "+exception).send();
-};
+// app.post = function(request,response,type,exception) {
+//     // Always turn an exception into a successful response
+//     response.clear().say("An error occured: "+exception).send();
+// };
 
 app.intent('Pick', function(request,response) {
 	console.log('intent Pick');
-    var number = request.slot('Shows');
-    response.say("You picked the show "+number);
+    var data = request.slot('Shows', 'not found');
+    response.say("You picked the show "+data);
   }
 );
 
@@ -39,13 +42,6 @@ express.post('/',function(req,res) {
     });
 });
 
-express.get('/',function(req,res) {
-  app.request(req.body)        // connect express to alexa-app
-    .then(function(response) { // alexa-app returns a promise with the response
-      res.json(response);      // stream it to express' output
-    });
-});
-
-var server = express.listen(process.env.PORT, function() { 
+var server = express.listen(process.env.PORT || 8833, function() { 
 	console.log("Listening on: " + server.address().address + server.address().port);
 });
